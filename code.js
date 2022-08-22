c175dc4ed6743bde46244570f30b3a9852bd03b0
uancamilo <uancamilo@gmail.com>
2022-08-22 10:50:00 -0500

    punto 1 pedir datos

--- code.js
+++ code.js
@@ -1,32 +1,34 @@
 /* --------------------------- NO TOCAR DESDE ACÁ --------------------------- */
 let datosPersona = {
-  nombre: "",
-  edad: 0,
-  ciudad: "",
-  interesPorJs: "",
+	nombre: "",
+	edad: 0,
+	ciudad: "",
+	interesPorJs: "",
 };
 
-const listado = [{
-    imgUrl: "https://huguidugui.files.wordpress.com/2015/03/html1.png",
-    lenguajes: "HTML y CSS",
-    bimestre: "1er bimestre",
-  },
-  {
-    imgUrl: "https://jherax.files.wordpress.com/2018/08/javascript_logo.png",
-    lenguajes: "Javascript",
-    bimestre: "2do bimestre",
-  },
-  {
-    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
-    lenguajes: "React JS",
-    bimestre: "3er bimestre",
-  },
+const listado = [
+	{
+		imgUrl: "https://huguidugui.files.wordpress.com/2015/03/html1.png",
+		lenguajes: "HTML y CSS",
+		bimestre: "1er bimestre",
+	},
+	{
+		imgUrl: "https://jherax.files.wordpress.com/2018/08/javascript_logo.png",
+		lenguajes: "Javascript",
+		bimestre: "2do bimestre",
+	},
+	{
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
+		lenguajes: "React JS",
+		bimestre: "3er bimestre",
+	},
 ];
 
 const profileBtn = document.querySelector("#completar-perfil");
 const materiasBtn = document.querySelector("#obtener-materias");
 const verMasBtn = document.querySelector("#ver-mas");
-const cambiarTema = document.querySelector('#cambiar-tema');
+const cambiarTema = document.querySelector("#cambiar-tema");
 
 profileBtn.addEventListener("click", renderizarDatosUsuario);
 materiasBtn.addEventListener("click", recorrerListadoYRenderizarTarjetas);
@@ -34,36 +36,35 @@ cambiarTema.addEventListener("click", alternarColorTema);
 /* --------------------------- NO TOCAR HASTA ACÁ --------------------------- */
 
 function obtenerDatosDelUsuario() {
-  /* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
- 
+	/* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
 
+	nombre = prompt("Nombre");
+	edad = parseInt(prompt("Año de nacimiento"));
+	ciudad = prompt("En que ciudad vives");
+	interesPorJs = confirm("Te interesa javaScript");
+	
+	console.log(datosPersona);
 
+	datosPersona.nombre = nombre;
+	datosPersona.edad = edad;
+	datosPersona.ciudad = ciudad;
+	datosPersona.interesPorJs = interesPorJs;
+
+	console.log(datosPersona);
 }
 
 function renderizarDatosUsuario() {
-  /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
-  obtenerDatosDelUsuario();
-  /* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
-  
-
-
+	/* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
+	obtenerDatosDelUsuario();
+	/* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
 }
 
-
 function recorrerListadoYRenderizarTarjetas() {
-  /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
-  
-
-
+	/* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
 }
 
 function alternarColorTema() {
-  /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
- 
-  
-
-
+	/* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
 }
 
 /* --------------------- PUNTO 5: Escribe tu codigo aqui --------------------- */
-
